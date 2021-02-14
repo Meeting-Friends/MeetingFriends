a37360672f4ac89ed3516e4eaf1828b8eead24e2
CubeGray <kimjh1716@hanmail.net>
2021-02-14 23:38:05 +0900

    Update SignUpController.java

--- src/main/java/meet/controller/SignUpController.java
+++ src/main/java/meet/controller/SignUpController.java
@@ -5,23 +5,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import meet.model.domain.entity.Member;
 import meet.service.MemberService;
 
-@Controller(value="SignUpController")
+@RestController(value="SignUpController")
 public class SignUpController {
 	@Autowired
 	MemberService memberservice;
 
 	@PostMapping("/signup")
 	public String loginCheck(@RequestBody Member inputmember,Model model) {		
+	
+		System.out.println(inputmember);
+	
 		if(memberservice.checkExistingMember(inputmember.getId())) {
 			inputmember.setClassification("general");
 			memberservice.insertMember(inputmember);	//DB에 저장
-			return "redirect:signin.html";	//회원가입 성공시, 로그인 페이지로 이동
-		}else {
-			return "foward:signup.html";	//회원가입가입 실패시, 이미 가입된 회원이거나 정보가 누락되었을 경우
 		}
+
+		return "Signin";	
 	}
 }
